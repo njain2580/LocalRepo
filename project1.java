bd972df102ec4b53356128ea4c93b6a3d9533082
njain2580 <njain2580@gmail.com>
2024-06-23 03:29:39 +0530

    Code edited

--- project1.java
+++ project1.java
@@ -1,5 +1,8 @@
 public class Tester{
     public static void main (String[] args){
-        System.out.println("Hello World");
+        int a = 5;
+        int b = 10;
+        int sum = a+b;
+        System.out.println(sum);
     }
 } 
