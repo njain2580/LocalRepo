0f5ef0b7807e6a597f29110723eab79fc20b38b9
njain2580 <njain2580@gmail.com>
2024-06-23 03:36:53 +0530

    Code Changed

--- project1.java
+++ project1.java
@@ -1,8 +1,5 @@
 public class Tester{
     public static void main (String[] args){
-        int a = 5;
-        int b = 10;
-        int sum = a+b;
-        System.out.println(sum);
+        System.out.println("Nitin");
     }
 } 
